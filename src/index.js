54e1b968f43f9a9fa55dcff4403b6df2a7d4c0e9
deMGoncalves <cleber.engineer@hotmail.com>
2025-05-22 11:59:10 -0300

    ci: estrutura do projeto

---
+++ src/index.js
@@ -0,0 +1,5 @@
+// import "@component";
+// import "@directive";
+// import "@layout";
+// import "@token";
+// import "@typography";
--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,35 @@
 import { resolve } from "node:path";
 import { defineConfig } from "vite";
+import terser from "@rollup/plugin-terser";
 
 export default defineConfig({
 	build: {
 		lib: {
 			entry: resolve(__dirname, "src/index.js"),
-			fileName: "std",
-			formats: ["cjs", "es"],
-			name: "std",
+			fileName: "x",
+			formats: ["cjs", "es", "iife"],
+			name: "x",
 		},
+		minify: false,
 		outDir: "dist",
+		rollupOptions: {
+			plugins: [
+				terser({
+					format: {
+						comments: false,
+					},
+				}),
+			],
+		},
+		sourcemap: true,
 	},
-
 	resolve: {
 		alias: {
+			"@component": resolve(__dirname, "packages/component"),
 			"@directive": resolve(__dirname, "packages/directive"),
+			"@layout": resolve(__dirname, "packages/layout"),
+			"@token": resolve(__dirname, "packages/token"),
+			"@typography": resolve(__dirname, "packages/typography"),
 		},
 	},
 });
