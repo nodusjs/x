dec3917cd707d190761b86b0c71aca68baf005c0
deMGoncalves <cleber.engineer@hotmail.com>
2025-05-22 22:21:27 -0300

    chore(text): delimita o tamanho maximo do texto

--- packages/typography/text/style.js
+++ packages/typography/text/style.js
@@ -4,13 +4,14 @@ export const style = (text) =>
 	css`
     :host {
       color: var(--text-${text.color}${text.onBrand ? "_on-brand" : ""});
-      display: inline;
+      display: inline-flex;
       font-family: var(--font-family-base);
       font-size: var(--font-size-text-${text.size});
       font-weight: var(--font-weight-${text.weight});
       line-height: var(--line-height-text-${text.size});
       text-align: ${text.align};
       text-wrap: ${text.wrap};
+      width: var(--paragraph-max-width);
     }
 
     :host:hover {
