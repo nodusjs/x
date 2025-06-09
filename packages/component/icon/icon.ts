b18b983d0dfb06bdc6b14059cc27d7a0d9b2e5ce
deMGoncalves <cleber.engineer@hotmail.com>
2025-06-09 09:46:12 -0300

    chore(icon): definicao do setter e getter psar o use

--- packages/component/icon/icon.ts
+++ packages/component/icon/icon.ts
@@ -2,6 +2,16 @@ import { define } from "@nodusjs/std/directive";
 import Echo from "@nodusjs/std/echo";
 
 @define("x-icon")
-class Icon extends Echo(HTMLElement) {}
+class Icon extends Echo(HTMLElement) {
+  #use;
+
+  get use() {
+    return (this.#use ??= "");
+  }
+
+  set use(value) {
+    this.#use = value;
+  }
+}
 
 export default Icon;
