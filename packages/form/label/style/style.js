2ef7ce3b54511175952573fdc8693785444e5a88
deMGoncalves <cleber.engineer@hotmail.com>
2025-06-09 11:07:03 -0300

    chore(label): alinhando os elementos

--- packages/form/label/style/style.js
+++ packages/form/label/style/style.js
@@ -4,9 +4,11 @@ export const style = () =>
   css`
     :host {
       color: var(--text-secondary);
+      display: inline-flex;
       font-family: var(--font-family-base);
       font-size: var(--font-size-text-sm);
       font-weight: var(--font-weight-medium);
+      gap: var(--spacing-sm);
       line-height: var(--line-height-text-sm); 
     }
   `;
