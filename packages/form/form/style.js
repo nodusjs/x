758c471a6bc136a13e8ecc87d35a33acaac3a0eb
deMGoncalves <cleber.engineer@hotmail.com>
2025-06-11 14:58:32 -0300

    chore(form): nao deve estilizar nada

--- packages/form/form/style.js
+++ packages/form/form/style.js
@@ -3,13 +3,7 @@ import { css } from "@nodusjs/std/dom";
 export const style = () =>
   css`
     :host {
-      form {
-        align-items: start;
-        box-sizing: border-box;
-        display: flex;
-        flex-direction: column;
-        gap: var(--spacing-lg);
-      }
+      display: block;
     }
 
     :host(:state(hidden)) {
