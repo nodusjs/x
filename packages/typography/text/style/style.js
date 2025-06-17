70d43fb06f9553f6646c09e0c1aaf5372b8fa4c3
deMGoncalves <cleber.engineer@hotmail.com>
2025-06-17 10:26:22 -0300

    refactor(typography): o host precisa seguir o text

--- packages/typography/text/style/style.js
+++ packages/typography/text/style/style.js
@@ -3,6 +3,9 @@ import { css } from "@nodusjs/std/dom";
 export const style = (text) =>
   css`
     :host {
+      box-sizing: border-box;
+      display: inline-flex;
+
       ${text.as} {
         color: var(--text-${text.color}${text.onBrand ? "_on-brand" : ""});
         display: inline-flex;
@@ -10,10 +13,12 @@ export const style = (text) =>
         font-size: var(--font-size-text-${text.size});
         font-weight: var(--font-weight-${text.weight});
         line-height: var(--line-height-text-${text.size});
+        margin: 0;
         max-width: var(--paragraph-max-width);
         text-align: ${text.align};
         text-wrap: ${text.wrap};
         transition: all 0.2s ease-out;
+        width: auto;
       }
     }
 
