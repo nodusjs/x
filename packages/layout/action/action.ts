eed16f5ce528730f4052a55dce27989ea90e3f5f
deMGoncalves <cleber.engineer@hotmail.com>
2025-06-20 11:37:49 -0300

    fix: nao importei o connected

--- packages/layout/action/action.ts
+++ packages/layout/action/action.ts
@@ -1,4 +1,4 @@
-import { define } from "@nodusjs/std/directive";
+import { connected, define } from "@nodusjs/std/directive";
 import { paint } from "@nodusjs/std/dom";
 import { component } from "./component";
 import { slottable } from "./interface";
