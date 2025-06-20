02527b3ee6a66f70a13bf0a2a9dd15b7f405e852
deMGoncalves <cleber.engineer@hotmail.com>
2025-06-20 11:56:10 -0300

    style: estilizacoa do codigo

--- packages/directive/include/component.js
+++ packages/directive/include/component.js
@@ -1,4 +1,4 @@
 import { html } from "@nodusjs/std/dom";
 import { textContent } from "./interface";
 
-export const component = (view) => html`${view[textContent]}`;
+export const component = (include) => html`${include[textContent]}`;
