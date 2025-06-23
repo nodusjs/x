a3052f1feb698323d94b39c167a81b303138c139
deMGoncalves <cleber.engineer@hotmail.com>
2025-06-23 10:27:51 -0300

    chore: coloca o controle do width no host

--- packages/typography/text/style.js
+++ packages/typography/text/style.js
@@ -5,20 +5,18 @@ export const style = (text) =>
     :host {
       box-sizing: border-box;
       display: inline-flex;
+      max-width: var(--paragraph-max-width);
 
       ${text.as} {
         color: var(--text-${text.color}${text.onBrand ? "_on-brand" : ""});
-        display: inline-flex;
         font-family: var(--font-family-base);
         font-size: var(--font-size-text-${text.size});
         font-weight: var(--font-weight-${text.weight});
         line-height: var(--line-height-text-${text.size});
         margin: 0;
-        max-width: var(--paragraph-max-width);
         text-align: ${text.align};
         text-wrap: ${text.wrap};
         transition: all 0.2s ease-out;
-        width: auto;
       }
     }
 
