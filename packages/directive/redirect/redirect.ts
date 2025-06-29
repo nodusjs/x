4e09ef6e0c87dd4cc41642f42e194e4c1e17d42c
deMGoncalves <cleber.engineer@hotmail.com>
2025-06-29 15:24:10 -0300

    feat(redirec): component para fazer redirect

---
+++ packages/directive/redirect/redirect.ts
@@ -0,0 +1,22 @@
+import { Headless } from "@mixin";
+import { attributeChanged, define } from "@nodusjs/std/directive";
+import Echo from "@nodusjs/std/echo";
+
+@define("x-redirect")
+class Redirect extends Echo(Headless(HTMLElement)) {
+  #href;
+
+  get href() {
+    return (this.#href ??= "#");
+  }
+
+  @attributeChanged("href")
+  set href(value) {
+    this.#href = value;
+  }
+
+  go() {
+    history.pushState({}, "", this.href);
+    return this;
+  }
+}
